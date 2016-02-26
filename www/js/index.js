6a6700b44daf2f6b5f69e9e486a10d69fee3f621
TechnicalEducationServices <rene.vogt-lowell@emc.com>
2016-02-26 13:02:58 -0500

    replaced <backend_contacts_url> with the actual IP
    We're not going to have students edit this line but will make a reference to it instead to reinforce that this is just 1 of 5 frontends tested so far: curl, python, Firefox, Firefox RESTClient, and mobile

--- www/js/index.js
+++ www/js/index.js
@@ -135,7 +135,7 @@ var app = {
     getContacts: function() {
     	
     	// Get ENTERPRISE Contacts
-    	$.getJSON("<backend_contacts_url>", function(contacts) {
+    	$.getJSON("http://192.168.1.23:8080/ETAPP-REST-1/contacts", function(contacts) {
     	    $("#enterprise_contacts_listview").empty();
     	    var items = [];
     	    var contactItem = "";
